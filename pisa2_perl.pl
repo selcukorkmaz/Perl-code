eba8e46e4cf05867b0173ad4c8e83505217feef9
Selçuk Korkmaz <selcukorkmaz@hotmail.com>
2015-10-19 16:09:27 -0700

    update

--- pisa2_perl.pl
+++ pisa2_perl.pl
@@ -3,19 +3,20 @@
 #use strict;
 #use warnings;
 
-system("source /Applications/ccp4-6.5/bin/ccp4.setup-sh");
+system("sudo ./BINARY.setup")
+system("source bin/ccp4.setup-sh")
 
 
-open (pdbFilesList, 'pdb.txt');
+open (pdbFilesList, '/home/ubuntu/Perl-code/pdb.txt');
 while (<pdbFilesList>) {
     chomp;
     ($name, $file) = split("\t");
     
     system("pisa $name -analyse $file");
-    system("pisa $name -list assem >  assemblises/assem_$name");
-    system("pisa $name -list interf > interfaces/interf_$name");
-    system("pisa $name -xml assem > assmebliesXML/assem_$name.xml");
-    system("pisa $name -xml interf > interfacesXML/interf_$name.xml");
+    system("pisa $name -list assem >  /home/ubuntu/pisa/assemblises/assem_$name");
+    system("pisa $name -list interf > /home/ubuntu/pisa/interfaces/interf_$name");
+    system("pisa $name -xml assem > /home/ubuntu/pisa/assmebliesXML/assem_$name.xml");
+    system("pisa $name -xml interf > /home/ubuntu/pisa/interfacesXML/interf_$name.xml");
     
 }
 #$x . ' ' . $y;
